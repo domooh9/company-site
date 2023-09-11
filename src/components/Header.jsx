7d26e4269193f0c42e71aaadf417fbd341a4b050
Domnic Omondi <domooh9@gmail.com>
2023-09-11 18:24:48 +0300

    adjusting node packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ const Header = () => {
   return (
     <header id="home">
       <div id="landing-header">
-        <div className="header">
+        <div className="header" id="typing-animation">
           <h1>ELEVATING TECHNOLOGY</h1>
           <h1>EMPOWERING</h1>
           <h1>LIVES</h1>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non,
-            eveniet.
+          <p className="typing-animation">
+            Transforming possibilities into reality through innovative
+            technology solutions.
           </p>
-          <a href="#our-services" className="btn btn-primary">
-            About us
+          <a href="#about-us" className="btn btn-primary">
+            Learn About Us
           </a>
           <a href="#our-services" className="btn btn-secondary">
-            Our Services
+            Explore Our Services
           </a>
         </div>
       </div>
